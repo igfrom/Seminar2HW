d2ab5dc5ccb01762122939a8adcbfcfe8b7732de
Igor From <ig.from@yandex.ru>
2022-11-20 02:15:52 +0700

    add task15

---
+++ task15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите день недели ");
+int numberA = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+if(numberA<6 && numberA>0)
+{
+    Console.WriteLine("Это будний день");
+}
+else if(numberA>5 && numberA<8)
+{
+    Console.WriteLine("Это выходной день");
+}
+else {
+    Console.WriteLine("Это не день недели");
+}
