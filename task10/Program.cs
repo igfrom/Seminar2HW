f9b71104ab329019cfd3c0409ec1a92b203accac
Igor From <ig.from@yandex.ru>
2022-11-20 02:00:06 +0700

    add task13

---
+++ task10/Program.cs
@@ -0,0 +1,6 @@
+﻿int number = new Random().Next(100,1000);
+Console.WriteLine(number);
+int number1 = number/100;
+int number3 = (number/10)-(number1*10);
+
+Console.WriteLine($"{number3}");
---
+++ task13/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите число для вывода третьей цифры заданного числа ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+string str = numberA.ToString();
+Console.WriteLine(str[2]);
